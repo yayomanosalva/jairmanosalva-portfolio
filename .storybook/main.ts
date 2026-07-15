4c214572b0d56e9762f0606fb0086f5cc69134cc
yayomanosalva <yayomanosalva@gmail.com>
2026-07-14 20:52:25 -0500

    Updates

---
+++ .storybook/main.ts
@@ -0,0 +1,13 @@
+import type { StorybookConfig } from 'storybook-react-rsbuild';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-docs"
+  ],
+  "framework": "storybook-react-rsbuild"
+};
+export default config;
